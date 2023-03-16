168f129f4fcf0acff6d19973c7a6c56be4a577c9
Lorenzo Sarti <lorenzo.sarti@unifi.it>
2023-03-16 12:12:52 +0100

    Created script to generate Experiments configuration file

---
+++ experiment_runner/generate_experiments_configuration.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+
+
+### Generates the experiments_config.json file ###
+
+# Init
+base_directory = Path.cwd() / "test_env"
+sequences_directory = base_directory / "dataset" / "sequences"
+patch_root_directory = base_directory / "patches"
+# Get info
+sequences = [seq for seq in sequences_directory.iterdir() if seq.is_dir()]
+patches = [patch for patch in patch_root_directory.iterdir() if patch.is_dir()]
+
+# Setup
+WARMUP_FRAMES = 10
+INJECTION_POINTS_PERCENTAGES = [33, 66]
+
+# Generate PATCHES experiments
+experiments = []
+for sequence in sequences:
+    # Get the steady sequence length (the sequence without the warmup frames)
+    camera_path = sequence / "image_0"
+    steady_sequence_len = len(list(camera_path.glob("*.png"))) - WARMUP_FRAMES
+    if steady_sequence_len <= 0:
+        raise IndexError(f"The sequence {sequence.name} is too short!")
+
+    for patch in patches:
+        patch_variants = len(list(patch.glob("*.png")))
+        for variant in range(patch_variants):
+            for injection_percentage in INJECTION_POINTS_PERCENTAGES:
+                # Calculate the injection point
+                injection_point = (steady_sequence_len * injection_percentage) // 100
+                # Create the experiment configuration
+                experiments.append({
+                    "experiment_name": f"exp_{str(len(experiments)+1).zfill(4)}",
+                    "sequence_name": f"{sequence.name}",
+                    "failure_type": f"{patch.name}",
+                    "failure_variant": variant,
+                    "injection_position": WARMUP_FRAMES + injection_point
+                })
+
+# Save
+content = json.dumps(experiments, indent=2, default=str)
+with open("test_env/experiments_config.json", "w") as f:
+    f.write(content)
+
+print(content)
+print(f"TOTAL EXPERIMENTS: {len(experiments)}")
+

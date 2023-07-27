f0cc997c8b72d2e6828b573a9afa02534cc96bdb
Lorenzo Sarti <lorenzo.sarti@unifi.it>
2023-07-27 23:35:31 +0200

    Refactored main

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ from utils.Timer import Timer
 # CONFIG
 SEED = 42069
 VO_MODEL = VOModel.RETRAINED
+VOLUME_PATH = Path("/data/puccetti/inj_volume/")
+SELECTED_EXPERIMENTS = ["04_nominal", "exp_0324"]   # None for ALL
 
 # SETUP
 random.seed(SEED)
 np.random.seed(SEED)
 env = EnvironmentParameters()
 # env.volume_root_directory = Path.cwd() / "experiment_runner" / "test_env"
-env.volume_root_directory = Path("/data/puccetti/inj_volume/")
+env.volume_root_directory = VOLUME_PATH
 env.sequences_directory = env.volume_root_directory / "dataset" / "sequences"
 env.patch_root_directory = env.volume_root_directory / "patches"
 
@@ -29,29 +31,29 @@ experiments_conf = env.volume_root_directory / "experiments_config.json"
 # Create Controller
 controller = ExperimentsController(env, experiments_conf)
 
-#### Run experiments #######
-# results = controller.runExperiments(VO_MODEL)
-# # Show results
-# for res in results:
-#     print(res)
+if SELECTED_EXPERIMENTS is None:
+    #### Run ALL experiments #######
+    results = controller.runExperiments(VO_MODEL)
+    # Show results
+    for res in results:
+        print(res)
+else:
+    #### Prepare SELECTED experiments #########
+    with Timer("Experiments Preparation"):
+        prepared = controller.prepareExperiments(SELECTED_EXPERIMENTS)
+    # Show prepared
+    print(" EXPERIMENTS PREPARATION ".center(40, "#"))
+    for res in prepared:
+        print(res)
+    #### Run SELECTED experiments #########
+    with Timer("Experiments Run"):
+        results = controller.runPreparedExperiments(prepared, VO_MODEL)
+    # Show results
+    print(" EXPERIMENTS RUN ".center(40, "#"))
+    for res in results:
+        print(res)
 
-#### Prepare and Run #########
-with Timer("Experiments Preparation"):
-    prepared = controller.prepareExperiments(["04_nominal", "exp_0089"])
-    # prepared = controller.prepareExperiments(["exp_0115"])
-
-# Show results
-print(" EXPERIMENTS PREPARATION ".center(40, "#"))
-for res in prepared:
-    print(res)
-
-with Timer("Experiments Run"):
-    results = controller.runPreparedExperiments(prepared, VO_MODEL)
-# Show results
-print(" EXPERIMENTS RUN ".center(40, "#"))
-for res in results:
-    print(res)
-# CREATE SUMMARY
+## CREATE SUMMARY ##
 print("Creating Summary...")
 res_m = ResultsManager(env, experiments_conf)
 res_m.createSummary(results)

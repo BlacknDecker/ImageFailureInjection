87ae8727e0b32924bfd037a924632dbff21dfa24
Lorenzo Sarti <lorenzo.sarti@unifi.it>
2023-07-28 12:54:44 +0200

    Added error recovery in main script

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from experiment_runner.EnvironmentParameters import EnvironmentParameters
 from experiment_runner.ExperimentsController import ExperimentsController
 from experiment_runner.ResultsManager import ResultsManager
 from experiment_runner.VOModel import VOModel
+from utils.TelegramNotifier import TelegramNotifier
 from utils.Timer import Timer
 
 
@@ -27,34 +28,45 @@ env.sequences_directory = env.volume_root_directory / "dataset" / "sequences"
 env.patch_root_directory = env.volume_root_directory / "patches"
 
 experiments_conf = env.volume_root_directory / "experiments_config.json"
+notifier = TelegramNotifier(Path.cwd() / "secrets.json")
 
-# Create Controller
-controller = ExperimentsController(env, experiments_conf)
+try:
+    # Create Controller
+    controller = ExperimentsController(env, experiments_conf)
 
-if SELECTED_EXPERIMENTS is None:
-    #### Run ALL experiments #######
-    results = controller.runExperiments(VO_MODEL)
-    # Show results
-    for res in results:
-        print(res)
-else:
-    #### Prepare SELECTED experiments #########
-    with Timer("Experiments Preparation"):
-        prepared = controller.prepareExperiments(SELECTED_EXPERIMENTS)
-    # Show prepared
-    print(" EXPERIMENTS PREPARATION ".center(40, "#"))
-    for res in prepared:
-        print(res)
-    #### Run SELECTED experiments #########
-    with Timer("Experiments Run"):
-        results = controller.runPreparedExperiments(prepared, VO_MODEL)
-    # Show results
-    print(" EXPERIMENTS RUN ".center(40, "#"))
-    for res in results:
-        print(res)
+    if SELECTED_EXPERIMENTS is None:
+        #### Run ALL experiments #######
+        results = controller.runExperiments(VO_MODEL)
+        # Show results
+        for res in results:
+            print(res)
+    else:
+        #### Prepare SELECTED experiments #########
+        with Timer("Experiments Preparation"):
+            prepared = controller.prepareExperiments(SELECTED_EXPERIMENTS)
+        # Show prepared
+        print(" EXPERIMENTS PREPARATION ".center(40, "#"))
+        for res in prepared:
+            print(res)
+        #### Run SELECTED experiments #########
+        with Timer("Experiments Run"):
+            results = controller.runPreparedExperiments(prepared, VO_MODEL)
+        # Show results
+        print(" EXPERIMENTS RUN ".center(40, "#"))
+        for res in results:
+            print(res)
 
-## CREATE SUMMARY ##
-print("Creating Summary...")
-res_m = ResultsManager(env, experiments_conf)
-res_m.createSummary(results)
-print("Done!")
+    ## CREATE SUMMARY ##
+    print("Creating Summary...")
+    res_m = ResultsManager(env, experiments_conf)
+    res_m.createSummary(results)
+    print("Done!")
+except Exception as e:
+    print(f"FATAL ERROR: {e}")
+    msg = f"*FATAL ERROR*\nThe Experiment Campaign terminated due to an exception!"
+    notifier.notify(msg)
+
+# Notify END
+notifier.notify("*Experimental Campaign Ended!*")
+# Done
+
